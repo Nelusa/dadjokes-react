ee69365c83659a5ba7cf78ca88610cc045bed653
Nela Letochová <nela.letochova.wxc@gmail.com>
2022-05-28 10:06:40 +0200

    Úkol č. 15 + dodatečná úprava class na className v rámci úkolu č. 14

--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,74 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import Joke from './Joke/index.jsx';
+import { jokes } from './jokes.js';
 
 const App = () => {
+  return (
+    <div className="container">
+      {jokes.map((joke) => {
+        return (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+createRoot(document.querySelector('#app')).render(<App />);
+
+//---------------------------------------------------//
+
+// Komponenta App z úkolu č. 14 (pro přehlednost ohledně změn mezi úkoly 14 a 15, i když je poznat z commitu - tam však CHYBÍ OPRAVA CLASS NA CLASSNAME --> opraveno v rámci úkolu č. 15)
+/*const App = () => {
   const [likesUp, setLikesUp] = useState(0);
   const [likesDown, setLikesDown] = useState(0);
 
   return (
     <div className="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
             <img
-              class="user-avatar"
+              className="user-avatar"
               src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
             />
-            <p class="user-name">Neroxx</p>
+            <p className="user-name">Neroxx</p>
           </div>
 
-          <p class="joke__text">
+          <p className="joke__text">
             The secret service isn't allowed to yell "Get down!" anymore when
             the president is about to be attacked. Now they have to yell
             "Donald, duck!"
           </p>
         </div>
-        <div class="joke__likes">
+        <div className="joke__likes">
           <button
             id="btn-up"
-            class="btn-like btn-like--up"
+            className="btn-like btn-like--up"
             onClick={() => {
               setLikesUp(likesUp + 1);
             }}
           ></button>
-          <span id="likes-up" class="likes-count likes-count--up">
+          <span id="likes-up" className="likes-count likes-count--up">
             {likesUp}
           </span>
           <button
             id="btn-down"
-            class="btn-like btn-like--down"
+            className="btn-like btn-like--down"
             onClick={() => {
               setLikesDown(likesDown + 1);
             }}
           ></button>
-          <span id="likes-down" class="likes-count likes-count--down">
+          <span id="likes-down" className="likes-count likes-count--down">
             {likesDown}
           </span>
         </div>
@@ -51,4 +77,4 @@ const App = () => {
   );
 };
 
-createRoot(document.querySelector('#app')).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />); */
